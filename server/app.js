75f034063e40f2a1f442c811053cc685cc8efac2
SharatSinha09 <sharat.sinha2002@gmail.com>
2025-04-26 12:43:48 +0530

    database connect

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const taskRoutes = require('./routes/taskRoutes');
@@ -7,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-scheduler', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
@@ -18,4 +19,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-scheduler', {
 app.use('/api/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
